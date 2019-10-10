503948a9a47e7d8350fdd869587c0eda279f39bf
Babbler <18856426129ik@gmail.com>
2019-10-10 17:27:30 +0800

    计数排序

--- java_/src/algorithm/CountSort.java
+++ java_/src/algorithm/CountSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CountSort {
 
     public static void main(String[] args) {
-        int[] arr = {0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9};
+        int[] arr = {0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 8, 9};
         int[] result = sort(arr);
         System.out.println(Arrays.toString(result));
     }
