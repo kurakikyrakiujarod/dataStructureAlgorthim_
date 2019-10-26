6f2c0564cbd91007918f9936e0989db1f7e4f19b
Babbler <18856426129ik@gmail.com>
2019-10-26 21:20:28 +0800

    InsertSort

---
+++ java_/src/com/atguigu/sort/InsertSort.java
@@ -0,0 +1,39 @@
+package com.atguigu.sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+
+    public static void insertSort(int[] array) {
+
+        for (int i = 1; i < array.length; i++) {
+
+            int insertValue = array[i];
+            int insertIndex = i - 1;
+
+            while (insertIndex >= 0 && array[insertIndex] > insertValue) {
+
+                array[insertIndex + 1] = array[insertIndex];
+                insertIndex--;
+
+            }
+
+            if (insertIndex + 1 != i) array[insertIndex + 1] = insertValue;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = new int[12];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100 + 1);
+        }
+
+        System.out.println(Arrays.toString(array));
+        insertSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
